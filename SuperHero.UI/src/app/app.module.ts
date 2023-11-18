ec0ad8c227b065bfcd672f4b57e3326a5cb41630
elyonousimohamed <melyonousi@redimpactarabia.com>
2023-11-18 14:12:10 +0100

    update

--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './components/container/container.component';
 import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
